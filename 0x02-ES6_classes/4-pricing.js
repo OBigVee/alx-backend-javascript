1abcb7bac1c64e883b2ce41bb3e14a11f066d59f
OBigVee <adedejivictor98@gmail.com>
2022-04-10 15:57:46 +0100

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from "./3-currency";
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+    get amount() {
+        return this._amount;
+    }
+    set setAmount(val) {
+        if (typeof(val) !== 'number') {
+            throw Error("Amount type is not number");
+        } else {
+            this_.amount = val;
+        }
+    }
+    get getCurrency() {
+        return this._currency;
+    }
+    set setCurrency(val) {
+        if (typeof(val) !== 'Currency') {
+            throw Error("not of the type Currency");
+        } else {
+            this.__currency = val;
+        }
+    }
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.getName} (${this._currency.getCode})`;
+
+    }
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
