e30724f720222c34c7748b4818471b40a177d769
OBigVee <adedejivictor98@gmail.com>
2022-04-11 15:45:45 +0100

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,11 @@
-import Car from "./10-car";
+import Car from './10-car';
+
 export default class EVCar extends Car {
     constructor(brand, motor, color, range) {
         super(brand, motor, color);
         this._range = range;
     }
 
-    get range() {
-        return this._range;
-    }
     cloneCar() {
         return this.constructor;
     }
