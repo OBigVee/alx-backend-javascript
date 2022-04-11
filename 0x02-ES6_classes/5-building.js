9e2aae96bd7813146e61bfaf6d820af476339752
OBigVee <adedejivictor98@gmail.com>
2022-04-11 14:09:15 +0100

    feat:
    - Add 5-building.js file
    module has an abstract class Building and a abstract method evacuationWarningMessage

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -6,14 +6,16 @@
 export default class Building {
     constructor(sqft) {
         this._sqft = sqft;
-        if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+        if (this.constructor !== Building &&
+            this.evacuationWarningMessage === undefined) {
+
             throw new Error("Class extending Building must override evacuationWarningMessage");
         }
     }
-    get getSqft() {
+    get sqft() {
         return this._sqft;
     }
-    set setSqft(val) {
+    set sqft(val) {
         if (typeof(val) !== 'number') {
             return "sqft must be a number";
         } else {
