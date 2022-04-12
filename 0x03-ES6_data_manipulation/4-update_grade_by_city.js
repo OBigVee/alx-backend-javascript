2469225d5944ffb97cf655c458085e556c547db9
OBigVee <adedejivictor98@gmail.com>
2022-04-12 21:10:34 +0100

    Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,21 @@
+// Create a function updateStudentGradeByCity that 
+// returns an array of students for a specific city with their new grade
+// It should accept a list of students (from getListStudents), 
+// a city(String), and newGrades(Array of“ grade” objects) 
+// as parameters.
+// newGrades is an array of objects with this format:
+
+//     {
+//         studentId: 31,
+//         grade: 78,
+//     }
+
+export default function updateStudentGradeByCity(studentArray, city, newGrade) {
+    const info = studentArray.map((student) => {
+        const grade = newGrade.filter((studentGrade) => studentGrade.studentId ===
+            studentArray.id).map((selectInfo) => selectInfo.grade)[0];
+        student['grade'] = grade || 'N/A';
+        return student;
+    });
+    return info.filter((obj) => obj.location === city);
+}
