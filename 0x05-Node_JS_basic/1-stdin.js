8972bf87ba9eafcb58f2c138d58952dd9c7f57c6
OBigVee <adedejivictor98@gmail.com>
2022-05-31 11:09:57 +0100

    feat:
    -Add 1-stdin.js file
    - script uses process to execute through commnad line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
-process.stdin.setEncoding("utf-8");
-const intro = "Welcome to Holberton School, what is your name?\n"
-const outro = "This important software is now closing";
-process.stdin.write(intro);
-process.stdin.on("readable",function (){
-    const name = process.stdin.read();
-    if (name !== null){
-        console.log(`your name is: ${name}`);
-    }
-    console.log(outro);
+const intro = 'Welcome to Holberton School, what is your name?\n'
+const outro = 'This important software is now closing\n'
+process.stdout.write(intro)
+
+process.stdin.setEncoding('utf-8')
+
+process.stdin.on('readable', function () {
+  const getName = process.stdin.read()
+  if (getName !== null) {
+    process.stdout.write(`your name is: ${getName}`)
+  }
 })
-// const name = process.stdin.write("Welcome to Holberton School," +
-//     " what is your name?"+"\n");
-// console.log(`your name is: ${name} \n`);
-// console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write(outro)
+})
