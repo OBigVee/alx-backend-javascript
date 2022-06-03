be355de7975326f034ed02df5c3e950e2574d341
OBigVee <adedejivictor98@gmail.com>
2022-06-03 23:30:33 +0100

    :heavy_check_mark: Create a small HTTP server with HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,6 +7,7 @@ const displayText = 'Hello Holberton School!'
 const app = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', displayText.length);
   res.write(Buffer.from(displayText));
 })
 app.listen(port, hostname, () => {
