1a20457dd1a244a0d9d5cb881e6d2c7219bc44ba
OBigVee <adedejivictor98@gmail.com>
2022-06-12 12:48:47 +0100

    :heavy_check_mark: Basic test with Mocha and Node assertion library Task completed :heavy_check_mark:

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+
+const assert = require('assert')
+const calculateNumber = require('./0-calcul')
+
+describe('calculateNumber', () => {
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4)
+  })
+
+  it('round down b float value', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 4)
+  })
+  it('round down a float value and b float value', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 4)
+  })
+
+  it('round down a float value and b float value', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 4)
+  })
+})
