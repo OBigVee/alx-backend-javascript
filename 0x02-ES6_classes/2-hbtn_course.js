38432d1bdbeafc685640523cf22cc0c4ff5b05c1
obigvee <adedejivictor98@gmail.com>
2023-12-24 20:46:49 +0100

    task 2 completed :wrench:

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,80 +1,71 @@
 /**
  * Represents a Holberton Course.
  */
-
 export default class HolbertonCourse {
-  /**
+    /**
      * Creates a new @see {@link HolbertonCourse}.
      *
      * @param {String} name - The name of the course.
      * @param {Number} length - How long the course is (in months).
      * @param {String[]} students - The names of students in the course.
      */
-  constructor (name, length, students) {
-    this._name = this._validateString(name)
-    this._length = this._validateLength(length)
-    this._students = this._validateStudents(students)
-  }
+    constructor(name, length, students) {
+        this.name = name;
+        this.length = length;
+        this.students = students;
+    }
 
-  /**
+    /**
      * Gets the name of this course.
      */
-  get name () {
-    return this._name
-  }
+    get name() {
+        return this._name;
+    }
 
-  /**
+    /**
      * Sets the name of this course.
      */
-  set name (newName) {
-    this._name = this._validateString(newName)
-  }
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = value;
+    }
 
-  /**
+    /**
      * Gets the length of this course (in months).
      */
-  get length () {
-    return this._length
-  }
+    get length() {
+        return this._length;
+    }
 
-  /**
+    /**
      * Sets the length of this course (in months).
      */
-  set length (newLen) {
-    this._length = this._validateLength(newLen)
-  }
+    set length(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        this._length = value;
+    }
 
-  /**
+    /**
      * Gets the names of students in this course.
      */
-  get students () {
-    return this._students
-  }
+    get students() {
+        return this._students;
+    }
 
-  /**
+    /**
      * Sets the names of students in this course.
      */
-  set students (newStudent) {
-    this._students = this._validateStudents(newStudent)
-  }
-
-  _validateString (val) {
-    if (typeof val !== 'string') {
-      throw new TypeError('Name must be a string')
+    set students(value) {
+        if (!(value instanceof Array)) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        if (!value.every((student) => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = value;
     }
-    return val
-  }
-
-  _validateLength (val) {
-    if (typeof val !== 'number') {
-      throw new TypeError('Length must be a valid number')
-    }
-  }
-
-  _validateStudents (val) {
-    if (!Array.isArray(val) || !val.every(item => typeof item === 'string')) {
-      throw new TypeError(' Students must be an array of strings')
-    }
-    return val
-  }
 }
