437b501114d03ba6216a10c3ff96929d22e2cb7d
obigvee <adedejivictor98@gmail.com>
2023-12-28 17:35:39 +0100

    task 7 completed :white_check_mark:

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -5,39 +5,39 @@
 export default class Airport {
   constructor (name, code) {
     if (typeof (name) !== 'string' && typeof (code) !== 'string') {
-      throw new TypeError('name must be type string,  code must be type number')
+      throw new TypeError('name must be type string,  code must be type number');
     }
 
-    this._name = name
-    this._code = code
+    this._name = name;
+    this._code = code;
   }
 
   get name () {
-    return this._name
+    return this._name;
   }
 
   get code () {
-    return this._code
+    return this._code;
   }
 
   set name (value) {
     if (typeof (value) !== 'string') {
-      throw new TypeError('value must be a type string.')
+      throw new TypeError('value must be a type string.');
     } else {
-      this._name = value
+      this._name = value;
     }
   }
 
   set code (value) {
     if (typeof (value) !== 'string') {
-      throw new TypeError('value must be a type a string.')
+      throw new TypeError('value must be a type a string.');
     } else {
-      this._code = value
+      this._code = value;
     }
   }
 
   toString () {
-    return `[object ${this._code}]`
+    return `[object ${this._code}]`;
   }
 }
 
