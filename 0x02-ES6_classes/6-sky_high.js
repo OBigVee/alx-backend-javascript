41147703888ec3754fde93f6f4aa0fbb0da06f5d
obigvee <adedejivictor98@gmail.com>
2023-12-28 14:09:06 +0100

    task 6 completed :white_check_mark:

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -6,6 +6,10 @@ import Building from './5-building.js'
 
 export default class SkyHighBuilding extends Building {
   constructor (sqft, floors) {
+    /**
+     * @sqft (Number)
+     * @floors (Number)
+     */
     super(sqft)
     if (typeof (floors) !== 'number' || floors <= 0) {
       throw Error('Invalid floors: floors  must be a positive number')
