b9f4182a73f0baa81a15d71d211e8bced92fc56c
obigvee <adedejivictor98@gmail.com>
2023-12-28 21:46:51 +0100

    task 10  :white_check_mark: and test passed

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,35 @@
+/**
+ * Vroom
+ */
+const brandSymbol = Symbol('brand')
+const motorSymbol = Symbol('motor')
+const colorSymbol = Symbol('color')
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this[brandSymbol] = brand
+    this[motorSymbol] = motor
+    this[colorSymbol] = color
+  }
+
+  get brand () {
+    return this[brandSymbol]
+  }
+
+  get motor () {
+    return this[motorSymbol]
+  }
+
+  get color () {
+    return this[colorSymbol]
+  }
+
+  cloneCar () {
+    // creating a new car object with the same properties
+    return new this.constructor(
+        this[brandSymbol],
+        this[motorSymbol],
+        this[colorSymbol]
+    );
+  }
+}
