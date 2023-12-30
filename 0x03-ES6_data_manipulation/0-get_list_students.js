bcb8a6aeb3a3d270643a9af0b717ba2215dad7d6
obigvee <adedejivictor98@gmail.com>
2023-12-30 15:11:44 +0100

    task 0 completed :heavy_check_mark: and test passed :white_check_mark:

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,25 @@
+/**
+ * basci list of objects
+ */
+
+export default function getListStudents () {
+  const arrObj = [{
+
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco'
+  },
+  {
+
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia'
+  },
+  {
+
+    id: 5,
+    firstName: 'Serana',
+    location: 'San Francisco'
+  }]
+  return arrObj
+}
