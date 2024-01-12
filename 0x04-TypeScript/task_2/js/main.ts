ddcef5e75f3faf2d5735b8b51a5e2df2df789936
obigvee <adedejivictor98@gmail.com>
2024-01-12 02:45:01 +0100

    :package:

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -94,11 +94,37 @@ class Teacher implements TeacherInterface{
     }
 }
 
-function createEmployee (salary: number | string): DirectorInterface | TeacherInterface{
+function createEmployee (salary: number | string): Director | Teacher{
     if ( typeof salary === "number" && salary < 500){
         return new Teacher();
     }else{
         return new Director();
 
+    }
+function isDirector(employee:Director | Teacher){
+    if (employee instanceof Director){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function executeWork(employee:Director | Teacher):string{
+    if (employee instanceof Director){
+        return (employee as Director).workDirectorTasks();
+    }else{
+       (employee as Teacher).workTeacherTasks();
+    }
+}
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass:Subjects){
+    if(todayClass === "Math" ){
+        return "Teaching Math";
+    }else{
+        return "Teaching History"
     }
 }
